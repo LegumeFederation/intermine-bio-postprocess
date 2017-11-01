27d29534ffbd34f8e58df1b40168c45ca84223f7
Sam Hokin <shokin@ncgr.org>
2017-11-01 11:37:11 -0600

    More efficient, only stores new annotations, using the commons-lang Pair class to keep track of ones already in the mine.

--- main/src/org/intermine/bio/postprocess/CreateOntologyParentAnnotations.java
+++ main/src/org/intermine/bio/postprocess/CreateOntologyParentAnnotations.java
@@ -15,13 +15,23 @@ import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Set;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.LinkedHashMap;
 
 import org.intermine.metadata.ConstraintOp;
 
 import org.intermine.model.bio.BioEntity;
+
 import org.intermine.model.bio.OntologyAnnotation;
+import org.intermine.model.bio.GOAnnotation;
+import org.intermine.model.bio.POAnnotation;
+import org.intermine.model.bio.TOAnnotation;
+
 import org.intermine.model.bio.OntologyTerm;
+import org.intermine.model.bio.GOTerm;
+import org.intermine.model.bio.POTerm;
+import org.intermine.model.bio.TOTerm;
 
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.objectstore.ObjectStoreWriter;
@@ -40,11 +50,15 @@ import org.intermine.objectstore.query.SimpleConstraint;
 
 import org.intermine.util.DynamicUtil;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.log4j.Logger;
 
 /**
  * For given ontology annotations, create additional annotations with those ontology terms' parents.
  * This allows one to only specify the deepest ontology term for, say, a QTL, but be able to query the mine for higher-level terms.
+ *
+ * NOTE: HARD-CODED LIMIT TO TO: terms for now
  *
  * @author Sam Hokin
  */
@@ -69,18 +83,6 @@ public class CreateOntologyParentAnnotations {
         
         LOG.info("Querying OntologyAnnotation records and associated terms and parents...");
 
-        // <class name="OntologyAnnotation" is-interface="true">
-	//   <reference name="ontologyTerm" referenced-type="OntologyTerm" reverse-reference="ontologyAnnotations"/>
-	//   <reference name="subject" referenced-type="BioEntity" reverse-reference="ontologyAnnotations"/>
-        // </class>
-
-        // <class name="OntologyTerm" is-interface="true">
-	//   <attribute name="identifier" type="java.lang.String"/>
-	//   <attribute name="name" type="java.lang.String"/>
-	//   <collection name="ontologyAnnotations" referenced-type="OntologyAnnotation" reverse-reference="ontologyTerm"/>
-	//   <collection name="parents" referenced-type="OntologyTerm"/>
-        // </class>
-        
         Query qOntologyAnnotation = new Query();
         qOntologyAnnotation.setDistinct(true);
         ConstraintSet csOntologyAnnotation = new ConstraintSet(ConstraintOp.AND);
@@ -90,24 +92,17 @@ public class CreateOntologyParentAnnotations {
         qOntologyAnnotation.addToSelect(qcOntologyAnnotation);
         qOntologyAnnotation.addFrom(qcOntologyAnnotation);
 
-        // 1 OntologyAnnotation.ontologyTerm
+        // 1 OntologyAnnotation.ontologyTerm (needed for parents)
         QueryClass qcOntologyTerm = new QueryClass(OntologyTerm.class);
         qOntologyAnnotation.addToSelect(qcOntologyTerm);
         qOntologyAnnotation.addFrom(qcOntologyTerm);
         QueryObjectReference ontologyTerm = new QueryObjectReference(qcOntologyAnnotation, "ontologyTerm");
         csOntologyAnnotation.addConstraint(new ContainsConstraint(ontologyTerm, ConstraintOp.CONTAINS, qcOntologyTerm));
 
-        // LIMIT TO TO: terms for now
-        csOntologyAnnotation.addConstraint(new SimpleConstraint(new QueryField(qcOntologyTerm,"identifier"), ConstraintOp.MATCHES, new QueryValue("TO:%")));
+        // DEV -- term identifier constraint, speeds things up for dev
+        // csOntologyAnnotation.addConstraint(new SimpleConstraint(new QueryField(qcOntologyTerm,"identifier"), ConstraintOp.MATCHES, new QueryValue("TO:%")));
 
-        // 2 OntologyAnnotation.subject
-        QueryClass qcSubject = new QueryClass(BioEntity.class);
-        qOntologyAnnotation.addToSelect(qcSubject);
-        qOntologyAnnotation.addFrom(qcSubject);
-        QueryObjectReference subj = new QueryObjectReference(qcOntologyAnnotation, "subject");
-        csOntologyAnnotation.addConstraint(new ContainsConstraint(subj, ConstraintOp.CONTAINS, qcSubject));
-
-        // 3 OntologyAnnotation.ontologyTerm.parents
+        // 2 OntologyAnnotation.ontologyTerm.parents
         QueryClass qcParents = new QueryClass(OntologyTerm.class);
         qOntologyAnnotation.addToSelect(qcParents);
         qOntologyAnnotation.addFrom(qcParents);
@@ -117,36 +112,67 @@ public class CreateOntologyParentAnnotations {
         // set the constraints
         qOntologyAnnotation.setConstraint(csOntologyAnnotation);
 
-        // begin transaction
-        osw.beginTransaction();
+        // store the existing subject/term id pairs so we don't duplicate them
+        Set<Pair<String,String>> subjectTermSet = new LinkedHashSet<Pair<String,String>>();
+        
+        // store the subject/parents in a Set of Pairs for insertion
+        Set<Pair<BioEntity,OntologyTerm>> subjectParentSet = new LinkedHashSet<Pair<BioEntity,OntologyTerm>>();
 
         // execute the query
         Results otResults = osw.getObjectStore().execute(qOntologyAnnotation);
         Iterator<?> otIter = otResults.iterator();
         while (otIter.hasNext()) {
             ResultsRow<?> rr = (ResultsRow<?>) otIter.next();
-            // objects
+            // result objects
             OntologyAnnotation annotation = (OntologyAnnotation) rr.get(0);
             OntologyTerm term = (OntologyTerm) rr.get(1);
-            BioEntity subject = (BioEntity) rr.get(2);
-            OntologyTerm parent = (OntologyTerm) rr.get(3);
-            // field values
-            String termIdentifier = (String) term.getFieldValue("identifier");
-            String termName = (String) term.getFieldValue("name");
+            OntologyTerm parent = (OntologyTerm) rr.get(2);
+            BioEntity subject = (BioEntity) annotation.getFieldValue("subject");
+            // store the existing annotation in a Set
             String subjectId = (String) subject.getFieldValue("primaryIdentifier");
-            String parentIdentifier = (String) parent.getFieldValue("identifier");
-            String parentName = (String) parent.getFieldValue("name");
-            // create and store a new ontology annotation with this subject and a new term from the parent
-            OntologyAnnotation newAnnotation = (OntologyAnnotation) DynamicUtil.createObject(Collections.singleton(OntologyAnnotation.class));
-            newAnnotation.setSubject(subject);
-            newAnnotation.setOntologyTerm(parent);
-            osw.store(newAnnotation);
-            // DEBUG
-            LOG.info(subjectId+"\t"+termIdentifier+":"+termName);
-            LOG.info("+\t\t"+parentIdentifier+":"+parentName);
+            String termIdentifier = (String) term.getFieldValue("identifier");
+            Pair<String,String> termPair = new MutablePair<String,String>(subjectId,termIdentifier);
+            subjectTermSet.add(termPair);
+            // store parent pairs in a Set
+            Pair<BioEntity,OntologyTerm> parentPair = new MutablePair<BioEntity,OntologyTerm>(subject,parent);
+            subjectParentSet.add(parentPair);
         }
 
-        // commit and close transaction
+        LOG.info("Storing new parent OntologyAnnotation records...");
+
+        // add each parent annotation that isn't already in the database
+        osw.beginTransaction();
+        for (Pair<BioEntity,OntologyTerm> pair : subjectParentSet) {
+            BioEntity subject = pair.getLeft();
+            OntologyTerm term = pair.getRight();
+            String subjectId = (String) subject.getFieldValue("primaryIdentifier");
+            String termIdentifier = (String) term.getFieldValue("identifier");
+            String termName = (String) term.getFieldValue("name");
+            // store if this pair is NOT already in database
+            Pair<String,String> termPair = new MutablePair<String,String>(subjectId,termIdentifier);
+            if (!subjectTermSet.contains(termPair)) {
+                // HACK: this shouldn't be hardcoded!
+                // add annotation subclass -- branch on type
+                if (termIdentifier.startsWith("GO:")) {
+                    GOAnnotation newAnnotation = (GOAnnotation) DynamicUtil.createObject(Collections.singleton(GOAnnotation.class));
+                    newAnnotation.setSubject(subject);
+                    newAnnotation.setOntologyTerm(term);
+                    osw.store(newAnnotation);
+                } else if (termIdentifier.startsWith("PO:")) {
+                    POAnnotation newAnnotation = (POAnnotation) DynamicUtil.createObject(Collections.singleton(POAnnotation.class));
+                    newAnnotation.setSubject(subject);
+                    newAnnotation.setOntologyTerm(term);
+                    osw.store(newAnnotation);
+                } else if (termIdentifier.startsWith("TO:")) {
+                    TOAnnotation newAnnotation = (TOAnnotation) DynamicUtil.createObject(Collections.singleton(TOAnnotation.class));
+                    newAnnotation.setSubject(subject);
+                    newAnnotation.setOntologyTerm(term);
+                    osw.store(newAnnotation);
+                } else {
+                    LOG.error("Unsupported OntologyTerm "+termIdentifier);
+                }
+            }
+        }
         osw.commitTransaction();
 
     }
